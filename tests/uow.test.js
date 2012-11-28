c5c6785426d35c2d9996d69d855c8af6d41c3e02
CyberLight <cyberlight@live.ru>
2012-11-28 22:09:54 +0600

    #3 * Added Unit of Work for aggregating all repositories * Added class uow and 'users' property, which return user's repository object * Added tests for uow, for checking that the valid data is returned

---
+++ tests/uow.test.js
@@ -0,0 +1,45 @@
+var mocha = require('../node_modules/mocha'),
+    mongodb = require('../node_modules/mongodb'),
+    should = require('../node_modules/should'),
+    UoW = require('../lib/data/uow').UoW,
+    Interface = require('../lib/interfaces/interface').jspatterns.contracts.Interface,
+    IUow = new Interface("IUow", ['users']),
+    uow = null,
+    server = null,
+    db = null;
+
+beforeEach(function(){        
+    server = new mongodb.Server("localhost", 27017, {auto_reconnect : true, poolSize : 4, socketOptions : {encoding : "utf-8"}});
+	db = new mongodb.Db(DB_NAME, server, {safe: true});
+	db.on("close", function(error){
+		console.log("--- connection closed successfully");
+	});
+    uow = new UoW(db);
+});
+    
+describe("Unit of Work tests", function(){
+
+    it("should created successfull", function(){                
+        should.exist(uow);
+    });
+    
+    it("should implementat IUow interface", function(){
+        (function(){
+            Interface.ensureImplements(uow, IUow);
+        }).should.not.throw();
+    });
+    
+    describe("'users' property of UoW", function(){
+    
+        it("should return users repository from 'users' of UoW", function(){
+            uow.users().collection.should.be.equal('users');
+        });
+        
+        it("should return users repository with setted db from", function(){
+            var returnedDb = uow.users().getDb();
+            should.exist(returnedDb);
+            returnedDb.should.be.equal(db);
+        });
+
+    });
+});
