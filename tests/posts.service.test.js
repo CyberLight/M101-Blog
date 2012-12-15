c1f521244acf44c254ad854a46cac0c1396cf35b
CyberLight <cyberlight@live.ru>
2012-12-15 23:01:44 +0600

    #10 * Posts service added to services class * Regenerated posts.json * Updated skip test for generate posts test data

--- tests/posts.service.test.js
+++ tests/posts.service.test.js
@@ -150,9 +150,20 @@ describe("posts service tests", function(){
         
         it.skip("gen 11 posts and export", function(done){
             importPostsData(function(){
-                for(var i = 0; i< 12; i++){
+                var bodyOfPost = "Contrary to popular belief, Lorem Ipsum is not simply random text. \
+It has roots in a piece of classical Latin literature from 45 BC, \
+making it over 2000 years old. Richard McClintock, a Latin professor \
+at Hampden-Sydney College in Virginia, looked up one of the more \
+obscure Latin words, consectetur, from a Lorem Ipsum passage, and \
+going through the cites of the word in classical literature, \
+discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \
+\"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC.\
+This book is a treatise on the theory of ethics, very popular during the Renaissance. \
+The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32."
+
+                for(var i = 0; i< 11; i++){
                     var post = {
-                                body : "Content content content" + i,
+                                body : "Post#" + i + ": " + bodyOfPost,
                                 author : "user1",
                                 title : "User1 post#" + i,
                                 tags : "tag" +(i+1)+ ", tag" + (i+2) + ", tag" + (i+3)
--- tests/services.test.js
+++ tests/services.test.js
@@ -1,6 +1,6 @@
 var Interface = require('../lib/interfaces/interface').jspatterns.contracts.Interface,
     should = require('../node_modules/should'),
-    IServices = new Interface('IServices', ['getUserService']),
+    IServices = new Interface('IServices', ['getUserService', 'getPostsService']),
     services = require('../lib/data/services').services;
     
 describe("services tests", function(){
@@ -17,6 +17,13 @@ describe("services tests", function(){
     it("should return instance of userService", function(done){
         services.getUserService(function(err, userService){
             should.exist(userService);
+            done();
+        });
+    })
+    
+     it("should return instance of postService", function(done){
+        services.getPostsService(function(err, postService){
+            should.exist(postService);
             done();
         });
     })
