c8fb148746b38d734388ccd67fc10322ef2eb0e5
CyberLight <cyberlight@live.ru>
2012-12-15 23:04:28 +0600

    #8 * Added displaying top 10 posts on main page * Added style for post section * Added tests for checking correctness of displaying posts * Added date.format.js for date formatting purposes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@
  * GET home page.
  */
 
- var isAuthenticated = false;
+var isAuthenticated = false,
+    querystring = require('querystring'),
+    services = require('../lib/data/services').services,
+    DateFormatter = require('../lib/utils/date.format').DateFormatter,
+    POST_BODY_PREVIEW_LEN = 337;
 
 function checkAuthentication(req, res){
    isAuthenticated = req.signedCookies.username ? true : false;
@@ -12,7 +16,18 @@ function checkAuthentication(req, res){
 function renderIndex(req, res){
     checkAuthentication(req, res);
     console.log('isAuthenticated : '+ isAuthenticated);
-    res.render('index', { title: 'M101 Blog', user : { isAuthenticated : isAuthenticated, name : req.signedCookies.username }});
+    services.getPostsService(function(err, postsService){
+        postsService.getTopPosts(function(err, posts){
+            posts.forEach(function(post){
+                post.title = querystring.unescape(post.title).toUpperCase();
+                post.viewlink = "/post/" + post.permalink + "/view";
+                if(post.body.length > POST_BODY_PREVIEW_LEN)
+                    post.body = post.body.substring(0, POST_BODY_PREVIEW_LEN-3) + "..."
+                post.publishDate = DateFormatter.format(post.date, "mmm, dd yyyy");
+            });
+            res.render('index', { title: 'M101 Blog', user : { isAuthenticated : isAuthenticated, name : req.signedCookies.username }, posts : posts});
+        });
+    });
 }
  
 exports.index = function(req, res){
--- tests/app.test.js
+++ tests/app.test.js
@@ -9,13 +9,26 @@ var mocha = require('../node_modules/mocha'),
 	agent = superagent.agent(),
 	httpRootPath = 'http://localhost:3000',
     DB_NAME = "M101Test",
-    COL_USERS = "users";
+    COL_USERS = "users",
+    COL_POSTS = "posts";
 	
 function importUsersData(cb){
      mDbImport.setDb(DB_NAME).setCollection(COL_USERS).setDrop(true).importData("tests\\importData\\users.json", function(code){
         code.should.be.equal(0);
         cb();
     });
+}
+
+function importPostsData(cb){
+     mDbImport.setDb(DB_NAME)
+              .setCollection(COL_POSTS)
+              .setDrop(true)
+              .importData("tests\\importData\\posts.json", 
+                            function(code){
+                                code.should.be.equal(0);
+                                cb();
+                            }
+                         );
 }
 
 function logError(err){
@@ -69,6 +82,62 @@ describe('Main page', function(){
                     $('a.login').text().should.include("Login");
                     $('a.signup').text().should.include("Sign up");
                     return done();
+                });
+            });
+            
+            it("should include no more then 10 most newest posts on page", function(done){
+                importUsersData(function(){
+                    importPostsData(function(){
+                        agent.get(httpRootPath).end(function(err,res){
+                            logError(err);
+                            var $ = cheerio.load(res.text);
+                            $('a.post-title').length.should.be.above(1);
+                            return done();
+                        });
+                    });
+                });
+            });
+            
+            it("should each href of post title contains valid link to post include permalink", function(done){
+                importUsersData(function(){
+                    importPostsData(function(){
+                        agent.get(httpRootPath).end(function(err,res){
+                            logError(err);
+                            var $ = cheerio.load(res.text);
+                            $('a.post-title').toArray().forEach(function(item){
+                                $(item).attr('href').should.match(/\/post\/([A-Za-z0-9]+)\/view/i);
+                            });
+                            return done();
+                        });
+                    });
+                });
+            });
+            
+            it("should contains 10 placeholders for short part of post entry", function(done){
+                importUsersData(function(){
+                    importPostsData(function(){
+                        agent.get(httpRootPath).end(function(err,res){
+                            logError(err);
+                            var $ = cheerio.load(res.text);
+                            $('section.post-preview').length.should.be.equal(10);
+                            return done();
+                        });
+                    });
+                });
+            });
+            
+            it("should contains short part of post entry in preview placeholder for each post", function(done){
+                importUsersData(function(){
+                    importPostsData(function(){
+                        agent.get(httpRootPath).end(function(err,res){
+                            logError(err);
+                            var $ = cheerio.load(res.text);
+                            $('section.post-preview').toArray().forEach(function(item){
+                                $(item).text().length.should.be.within(5, 337);
+                            });
+                            done();
+                        });
+                    });
                 });
             });
         });
